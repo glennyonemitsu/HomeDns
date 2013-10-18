1c4491bc75b2c55c06bb75c72133972810b74c65
Glenn Yonemitsu <yonemitsu@gmail.com>
2013-10-18 01:52:37 -0400

    simplified everything to a very small functional scope. currently only HTTP support is in

--- homedns.go
+++ homedns.go
@@ -2,22 +2,53 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
+	"flag"
+	"net"
+	"strings"
+	"strconv"
 )
 
-var db database
+var db map[string]*record
+var flagBindHttp *string
+var flagBindDns *string
+var flagPassword *string
+
+type record struct {
+	name	string
+	ttl		int64
+	ipv4	net.IP
+}
+
+func init() {
+	db = make(map[string]*record)
+
+	flagBindHttp = flag.String("bind_http", "0.0.0.0:8053", "IP:PORT to bind HTTP server to")
+	flagBindDns = flag.String("bind_dns", "0.0.0.0:53", "IP:PORT to bind DNS server to")
+	flagPassword = flag.String("password", "", "password to be used for HTTP client authentication")
+	flag.Parse()
+
+}
 
 func main() {
 	http.HandleFunc("/", router)
-	err := http.ListenAndServe("0.0.0.0:8081", nil)
+	err := http.ListenAndServe(*flagBindHttp, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func init() {
-	db = database{records: make(map[string]*record)}
-	db.setRecord("glenn", net.ParseIP("127.0.0.1"), net.ParseIP(""), 3600, "password")
-	db.setRecord("platters", net.ParseIP("192.168.1.124"), net.ParseIP(""), 3600, "password")
+func router(res http.ResponseWriter, req *http.Request) {
+	if password := req.FormValue("password"); password == *flagPassword {
+		rec := new(record)
+		rec.name = req.FormValue("name")
+		rec.ttl, _ = strconv.ParseInt(req.FormValue("ttl"), 10, 64)
+		rec.ipv4 = net.ParseIP(ipFromAddress(req.RemoteAddr))
+		db[rec.name] = rec
+	}
+}
+
+func ipFromAddress(address string) string {
+	parts := strings.Split(address, ":")
+	return parts[0]
 }
