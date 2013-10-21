f32f8f655735a12a97f8d503fb020e0411b16fa1
Glenn Yonemitsu <yonemitsu@gmail.com>
2013-10-20 21:32:24 -0400

    UDP only server method used. only accepts updates atm

--- homedns.go
+++ homedns.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"flag"
 	"net"
 	"strings"
@@ -10,9 +9,10 @@ import (
 )
 
 var db map[string]*record
-var flagBindHttp *string
-var flagBindDns *string
+var flagBind *string
 var flagPassword *string
+var homeDNSPrefix string
+var homeDNSPrefixLength int
 
 type record struct {
 	name	string
@@ -23,32 +23,60 @@ type record struct {
 func init() {
 	db = make(map[string]*record)
 
-	flagBindHttp = flag.String("bind_http", "0.0.0.0:8053", "IP:PORT to bind HTTP server to")
-	flagBindDns = flag.String("bind_dns", "0.0.0.0:53", "IP:PORT to bind DNS server to")
-	flagPassword = flag.String("password", "", "password to be used for HTTP client authentication")
+	flagBind = flag.String("bind", "0.0.0.0:53", "IP:PORT to bind to")
+	flagPassword = flag.String("password", "", "password to be used for authentication")
 	flag.Parse()
+
+	homeDNSPrefix = "HOMEDNS;" + *flagPassword + ";"
+	homeDNSPrefixLength = len(homeDNSPrefix)
 
 }
 
 func main() {
-	http.HandleFunc("/", router)
-	err := http.ListenAndServe(*flagBindHttp, nil)
+	ip, port := ipPort(*flagBind)
+	netIP := net.ParseIP(ip)
+	addr := net.UDPAddr{IP: netIP, Port: port, Zone: ""}
+	conn, err := net.ListenUDP("udp", &addr)
 	if err != nil {
 		fmt.Println(err)
 	}
-}
-
-func router(res http.ResponseWriter, req *http.Request) {
-	if password := req.FormValue("password"); password == *flagPassword {
-		rec := new(record)
-		rec.name = req.FormValue("name")
-		rec.ttl, _ = strconv.ParseInt(req.FormValue("ttl"), 10, 64)
-		rec.ipv4 = net.ParseIP(ipFromAddress(req.RemoteAddr))
-		db[rec.name] = rec
+	for {
+		handleUDPConnection(conn)
 	}
 }
 
-func ipFromAddress(address string) string {
+func ipPort(address string) (ip string, port int) {
 	parts := strings.Split(address, ":")
-	return parts[0]
+	ip = parts[0]
+	port64, _ := strconv.ParseInt(parts[1], 10, 0)
+	port = int(port64)
+
+	return ip, port
+}
+
+func handleUDPConnection(conn *net.UDPConn) {
+	input := make([]byte, 1024)
+	size, source, err := conn.ReadFromUDP(input[0:])
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		if string(input[0:homeDNSPrefixLength]) == homeDNSPrefix {
+			message := string(input[homeDNSPrefixLength:size])
+			messageParts := strings.Split(message, ";")
+			rec := new(record)
+			rec.name = messageParts[0]
+			rec.ttl, err = strconv.ParseInt(messageParts[1], 10, 64)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if len(messageParts) > 2 && strings.TrimSpace(messageParts[2]) != "" {
+				rec.ipv4 = net.ParseIP(messageParts[2])
+			} else {
+				rec.ipv4 = source.IP
+			}
+			db[messageParts[0]] = rec
+		} else {
+		}
+	}
 }
